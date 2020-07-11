e85115a43e0ddbf07c2fdc7ab82817d834c10acb
aks5bx <aks5bx@virginia.edu>
2020-07-11 19:15:52 -0400

    commenting out code, adding F1 score information

--- dataPrep.py
+++ dataPrep.py
@@ -1,19 +1,23 @@
+## Import library for data handling
 import pandas as pd
 
+## Read in two data frames 
+## matchups - all of the matchup data, so we know which team played which and who won
+## fullData - the data for each team and their aggregate statistics
 matchups = pd.read_csv('matchups.csv')
 fullData = pd.read_csv('fullData.csv')
 
+## Some dataframe handling
 matchups = matchups.dropna()
 matchups = matchups.reset_index()
 
-# print(matchups.iloc[396])
-
+## Define a function to retrieve a team's stats based on the year and team name
 def getTeamStats(team, year): 
     newDF = fullData[((fullData['Year'] == year) & (fullData['Team'] == team))]
     for index, row in newDF.iterrows():
         return list(row)
 
-
+## Test of the function
 print(getTeamStats('iona', 2019))
 
 
@@ -24,26 +28,34 @@ print(getTeamStats('iona', 2019))
 # allCols = allCols + allCols
 
 # regressionData = pd.DataFrame(columns = allCols)
+
+## Generating an empty list for collection purposes 
 regressionData = []
 
+## Declaring variables outside of the loop to track progress of the loop
 failureTotal = 0
 successTotal = 0
 i = 0
 
-## Use the csv of teams and their stats to produce regression ready csv 
-## the csv will have winning team and the losing team on the same line
+## Using the csv of teams and their stats (matchups and fullData) to produce data that is usable for a regression/other ML algorithm 
+## the csv will have winning team and the losing team on the same line, with the result as its own column
+## For each row in matchups
 for index, row in matchups.iterrows():
+    ## Because of the way the data is formatted - every third row we are ready to read in a new matchup
     if i % 3 == 0: 
-
+        
+        ## The first team is always the winner, so we grab that first
         year = int(row['Year'])
         winner = row['Team'].strip()
         winnerInfo = getTeamStats(winner, year)
 
+        ## The next row will be the losing team
         nextRow = matchups.iloc[index + 1]
 
         loser = nextRow['Team'].strip()
         loserInfo = getTeamStats(loser, year)            
 
+        # Combining the data into one row and append it to our list of matchups
         try:
             totalData = winnerInfo + loserInfo 
             regressionData.append(totalData)
@@ -54,12 +66,15 @@ for index, row in matchups.iterrows():
 
     i += 1
 
- 
+## Report the accuracy of this process - there may be some edge case failures
 print(round(failureTotal / (failureTotal + successTotal), 4) * 100, '%')
 
+## Transform list of matchups into a dataframe, write it to a csv
 regressionDF = pd.DataFrame.from_records(regressionData)
 regressionDF.to_csv('regressionData.csv')
 
-## REGRESSIONDATA gets changed and manually cleaned to turn into REGRESSIONUSEDATA
+
+## NOTE FOR THE END TO END PROCESS: 
+## regressionData gets changed and manually cleaned to turn into regressionUseData 
 
 
--- webscraper.py
+++ webscraper.py
@@ -1,34 +1,33 @@
+## Importing libraries for webscraping and dataframe handling
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-## Define DataFrame
+## This is the website from where statistics are being pulled
 ## https://www.teamrankings.com/ncb/stats/
 
+## Defining function to retrieve a year's march madness teams
 def getTeamsForYear(year): 
     filename = str(year) + '.csv' 
     yearDF = pd.read_csv(filename)
     teams = list(yearDF['Team'])
     return teams
 
-def listDiff(li1, li2): 
-    res = list(set(li1) - set(li2))
-    return res
 
 ## Get list of stats to scrape
 statList = ['effective-field-goal-pct', 'ftm-per-100-possessions', 'offensive-rebounding-pct', 'defensive-rebounding-pct', 'assists-per-fgm', 'effective-possession-ratio', 
             'RPI', 'win-pct-all-games', 'win-pct-close-games', 'SOS', 'Last 10', 'average-scoring-margin',  'offensive-efficiency', 'AdjO', 'defensive-efficiency', 'AdjD','EffM', 'AdjEM', 'points-per-game', 'opponent-points-per-game', 'PFAM', 'true-shooting-percentage', 'opponent-true-shooting-percentage', 'TS%M', 'shooting-pct', 'opponent-shooting-pct', 'FG%M', 'three-point-pct', 'free-throw-pct', 'total-rebounding-percentage', 'steals-perpossession', 'turnovers-per-possession', 'opponent-turnovers-per-possession', 'TOM', 'block-pct', 'personal-fouls-per-possession']
 
-newCols = ['RPI', 'Win%', 'CGWin%', 'SOS', 'Last 10', 'Margin', 'OffE', 'AdjO', 'DefE', 'AdjD', 'EffM', 'AdjEM', 'PF', 'PA', 'PFAM', 'TrueS%', 'OpTS%', 'TS%M', 'FG%', 'OpFG%', 'FG%M', '3P%', 'FT%', 'RB%', 'ST/Pos', 'TO/Pos', 'OpTO/Pos', 'TOM', 'BL%', 'PF/Pos']
-
-
+## Adding year and team as columns, creating a dataframe with a column for each stat
 columnsToUse = ['Year', 'Team'] + statList
 collectorDF = pd.DataFrame(columns = columnsToUse)
 
+## Looping through each year for which we have data (2008 - 2019)
 for year in range(2008, 2020): 
-    ## Get relevant teams for this year
+    ## Creating a new variable for year, technical debt
     currentYear = year
-
+    
+    ## Grabbing all the march madness teams of this year
     teamsThisYear = getTeamsForYear(currentYear)
     teamsThisYear = teamsThisYear[:-1]
 
@@ -36,24 +35,26 @@ for year in range(2008, 2020):
     columnsToUse = ['Year', 'Team'] + statList
     yearDF = pd.DataFrame(columns = columnsToUse)
 
-
-    ## Determine the year we are looking at 
+    ## Status update for the Loop 
     print('CURRENT YEAR :', currentYear)
 
-    ## Website stats lags a year so year = year -1 
     yearDF['Team'] = teamsThisYear; yearDF['Year'] = currentYear
 
-    ## Determine the stat that we want to grab
+    ## Loop through all the stats we want to collect
     for stat in statList: 
+        ## Status update for loop
         print(stat)
+        
         ## Define Dictionary
         statDict = {}
+        
         ## Webscrape 
         year = str(year)
         URL = 'https://www.teamrankings.com/ncaa-basketball/stat/' + stat + '?date=' + year + '-04-10'
         page = requests.get(URL)
         soup = BeautifulSoup(page.content, 'html.parser')
         table_body=soup.find('tbody')
+        
         try: 
             rows = table_body.find_all('tr')
         except:
@@ -61,52 +62,26 @@ for year in range(2008, 2020):
             continue
 
         ## Parse data
-        success = False
         for row in rows:
             cols=row.find_all('td')
             cols=[x.text.strip() for x in cols]
-
+            
+            ## If we are looking at data from a march madness team for this year, add it to our DF
             if cols[1] in teamsThisYear: 
                 statistic = cols[2]
                 yearDF.loc[yearDF.Team == cols[1], stat] = statistic
 
-    # yearDF.to_csv('2019Check.csv')
-
-
+    ## Append the current year's dataframe to our main dataframe to collect information for each year
     collectorDF = collectorDF.append(yearDF)
 
-    if year == 2019: 
-        print(yearDF)
 
+## Write dataframe to csv
 collectorDF.to_csv('collectorDF.csv')
 
-## COLLECTORDF will have some data manually added, and will eventually become fullData
+## NOTE FOR THE END TO END PROCESS: 
+## collectorDF will have some data manually added, and will eventually become fullData
 
-'''
 
-newCols = ['RPI', 'Win%', 'CGWin%', 'SOS', 'Last 10', 'Margin', 'OffE', 'AdjO', 'DefE', 'AdjD', 'EffM', 'AdjEM', 'PF', 'PA', 'PFAM', 'TrueS%', 'OpTS%', 'TS%M', 'FG%', 'OpFG%', 'FG%M', '3P%', 'FT%', 'RB%', 'ST/Pos', 'TO/Pos', 'OpTO/Pos', 'TOM', 'BL%', 'PF/Pos']
-
-allCols = columnsToUse + newCols
-
-collector2 = pd.DataFrame(columns = allCols)
-
-# print(collectorDF.loc[collectorDF['Year'] == 2008])
-
-for year in range(2019, 2020): 
-    print(year)
-    filename = str(year) + '.csv'
-    currentYearDF = pd.read_csv(filename)
-    yearMergeDF = collectorDF[(collectorDF['Year'] == year)]
-
-    # res = yearMergeDF.merge(currentYearDF)
-    res = pd.merge(left = yearMergeDF, right = currentYearDF, how='outer', left_on = 'Team', right_on = 'Team')
-
-    print(collector2.columns)
-
-    collector2 = collector2.append(res)  
-
-collector2.to_csv('fullData2.csv')
-'''
 
 
 
